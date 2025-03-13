86aa47c0bf8617af5e334d3c55f70ca90494cb54
arielpenna <arielpenna@gmail.com>
2025-03-13 00:08:09 -0300

    feature/feat-333: Modifica archivo: BORRAR/prueba.js. En BORRAR/prueba.js: Modifica lógica condicional.

--- BORRAR/prueba.js
+++ BORRAR/prueba.js
@@ -3,19 +3,7 @@ function sumar(num1, num2) {
     console.log(`La suma de ${num1} más ${num2} es igual a ${resultado}`);
 }
 
-// Ejemplo de uso
-sumar(5, 3);
-function restar(num1, num2) {
-    const resultado = num1 - num2;
-    console.log(`La resta de ${num1} menos ${num2} es igual a ${resultado}`);
-}
 
-// Ejemplo de uso
-restar(5, 3);
-function multiplicar(num1, num2) {
-    const resultado = num1 * num2;
-    console.log(`La multiplicación de ${num1} por ${num2} es igual a ${resultado}`);
-}
 
 // Ejemplo de uso
 multiplicar(5, 3);
