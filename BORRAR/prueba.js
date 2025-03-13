83972a816938c4e32f06d56ee71436b848b4f431
arielpenna <arielpenna@gmail.com>
2025-03-12 23:57:57 -0300

    bugfix/BF-44: se agrego la funciond e resta

--- BORRAR/prueba.js
+++ BORRAR/prueba.js
@@ -5,3 +5,10 @@ function sumar(num1, num2) {
 
 // Ejemplo de uso
 sumar(5, 3);
+function restar(num1, num2) {
+    const resultado = num1 - num2;
+    console.log(`La resta de ${num1} menos ${num2} es igual a ${resultado}`);
+}
+
+// Ejemplo de uso
+restar(5, 3);
