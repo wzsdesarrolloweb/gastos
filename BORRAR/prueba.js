ce1f35605308534fa1a3374d9d5fd8bdcb6eaf2b
arielpenna <arielpenna@gmail.com>
2025-03-13 00:04:15 -0300

    bugfix/bf-55: Agrega archivo: BORRAR/demostracion.js. Modifica archivo: BORRAR/prueba.js. En BORRAR/demostracion.js: Añade/modifica 1 función(es). Modifica lógica condicional. Actualiza 3 comentario(s). En BORRAR/prueba.js: Añade/modifica 2 función(es). Modifica lógica condicional. Actualiza 2 comentario(s).

--- BORRAR/prueba.js
+++ BORRAR/prueba.js
@@ -12,3 +12,18 @@ function restar(num1, num2) {
 
 // Ejemplo de uso
 restar(5, 3);
+function multiplicar(num1, num2) {
+    const resultado = num1 * num2;
+    console.log(`La multiplicación de ${num1} por ${num2} es igual a ${resultado}`);
+}
+
+// Ejemplo de uso
+multiplicar(5, 3);
+
+function dividir(num1, num2) {
+    const resultado = num1 / num2;
+    console.log(`La división de ${num1} entre ${num2} es igual a ${resultado}`);
+}
+
+// Ejemplo de uso
+dividir(5, 3);
